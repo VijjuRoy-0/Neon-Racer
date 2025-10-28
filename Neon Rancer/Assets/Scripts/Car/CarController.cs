a8355a89c3ca55106162ba511e22f7d7c64569ba
VijjuRoy-0 <vijjuroy.vk@gmail.com>
2025-10-28 21:56:18 +0530

    Day 1

--- Neon Rancer/Assets/Scripts/Car/CarController.cs
+++ Neon Rancer/Assets/Scripts/Car/CarController.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Wheels Trasform")]
+    public Transform frontLeftWheels;
+    public Transform frontRightWheels;
+    public Transform rareLeftWheels;
+    public Transform rareRightWheels;
+
+    [Header("Wheels Colliders")]
+    public WheelCollider frontLeftWheelCollider;
+    public WheelCollider frontRightWheelCollider;
+    public WheelCollider rearLeftWheelCollider;
+    public WheelCollider rearRightWheelCollider;
+
+    [Header("Car Setting")]
+    public float maxtorque;
+    public float maxsteeringAngle;
+    public float brakeTorque;
+
+    float horizontalInput, verticalInput;
+    float torque,steeringAngle;
+    
     void Start()
     {
         
@@ -13,6 +32,55 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+    void Movement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //Torque
+        torque = maxtorque * verticalInput;
+       frontLeftWheelCollider.motorTorque = torque ;
+        frontRightWheelCollider.motorTorque = torque;
+        rearLeftWheelCollider.motorTorque = torque;
+        rearRightWheelCollider.motorTorque = torque;
+
+        // Steering
+        steeringAngle = maxsteeringAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = steeringAngle;
+        frontRightWheelCollider.steerAngle =steeringAngle;
+
+        //HandBrake
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rearLeftWheelCollider.brakeTorque = brakeTorque;
+            rearRightWheelCollider.brakeTorque = brakeTorque;
+            frontLeftWheelCollider.brakeTorque = brakeTorque;
+            frontRightWheelCollider.brakeTorque = brakeTorque;
+        }
+        else
+        {
+            rearLeftWheelCollider.brakeTorque = 0;
+            rearRightWheelCollider.brakeTorque = 0;
+            frontLeftWheelCollider.brakeTorque = 0;
+            frontRightWheelCollider.brakeTorque = 0;
+        }
+
+        WheelsPos(frontRightWheelCollider,frontRightWheels);
+        WheelsPos(frontLeftWheelCollider,frontLeftWheels);
+        WheelsPos(rearLeftWheelCollider,rareLeftWheels);
+        WheelsPos(rearRightWheelCollider,rareRightWheels);
+
+
+    }
+    void WheelsPos(WheelCollider _wheelCollider,Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+
+        _wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.position = pos;
+        wheelTransform.rotation = rot;
     }
 }
